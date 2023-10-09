aee0c0df8c408fc35dc304f656a1577166037e5b
Apple <apple@matts-pro.home>
2023-10-09 16:13:24 +0100

    functionality to reset board and grid sizing fixed

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-const target = document.querySelector('#container');
-const grid = document.querySelector('#gridsize')
-const submit = document.querySelector('#submit')
+const target = document.querySelector("#container");
+const grid = document.querySelector("#gridsize");
+const submit = document.querySelector("#submit");
+const reset = document.querySelector('#reset');
+const resetSquare = document.querySelector('.paint')
 
 
-const squareWidth = 45;
 function etch(gridsize) {
-    target.style.display = 'grid';
-target.style.gridTemplateColumns =  `repeat(${gridsize}, 45px)`;
-for (let i = 0; i < gridsize * gridsize; i++) {
-    const div = document.createElement('div'); 
-    div.classList.add('paint')
-    div.style.border = '2px solid black';
+  const squareWidth = Math.floor(1200/gridsize);
+  console.log(squareWidth)
+  target.style.display = "grid";
+  target.style.gridTemplateColumns = `repeat(${gridsize}, ${squareWidth}px)`;
+  for (let i = 0; i < gridsize * gridsize; i++) {
+    const div = document.createElement("div");
+    div.classList.add("paint");
+    div.style.border = "2px solid black";
     div.style.width = `${squareWidth}px`;
     div.style.height = `${squareWidth}px`;
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = 'blue';
-    })
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "blue";
+    });
+    reset.addEventListener("click", () => {
+        div.style.backgroundColor = "white";
+      });
+      div.addEventListener("click", () => {
+        div.style.backgroundColor = "white";
+      });
 
     target.appendChild(div);
+  }
 }
 
-}
-
-  submit.addEventListener('click', () => {
-   const gridsize = grid.value
-   etch(gridsize)
-  })
+submit.addEventListener("click", () => {
+  const gridsize = grid.value;
+  etch(gridsize);
+});
