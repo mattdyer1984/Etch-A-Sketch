29a873ff2c5ca040c3043db0a98090e5a8aa52c3
Apple <apple@matts-pro.home>
2023-10-09 15:44:32 +0100

    functionality to make squares change colour on hover

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const target = document.querySelector('#container');
+const grid = document.querySelector('#gridsize')
+const submit = document.querySelector('#submit')
 
-target.style.display = 'grid';
-target.style.gridTemplateColumns =  'repeat(16, 45px)';
-const numRows = 16;
-const numCols = 16;
 
 const squareWidth = 45;
-
-for (let i = 0; i < numRows * numCols; i++) {
+function etch(gridsize) {
+    target.style.display = 'grid';
+target.style.gridTemplateColumns =  `repeat(${gridsize}, 45px)`;
+for (let i = 0; i < gridsize * gridsize; i++) {
     const div = document.createElement('div'); 
+    div.classList.add('paint')
     div.style.border = '2px solid black';
     div.style.width = `${squareWidth}px`;
     div.style.height = `${squareWidth}px`;
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'blue';
+    })
 
     target.appendChild(div);
 }
+
+}
+
+  submit.addEventListener('click', () => {
+   const gridsize = grid.value
+   etch(gridsize)
+  })
