e64526e2fef4e5e66070c8115b6c2bf9793ec591
Apple <apple@matts-pro.home>
2023-10-09 14:44:36 +0100

    grid created

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const target = document.querySelectorAll('#container')
-for(let i = 0; i < 256; i++) {
-    target.innerHTML = "<div style='border:2px solid black; width:25px; height:25px;></div>"
+const target = document.querySelector('#container');
+
+target.style.display = 'grid';
+target.style.gridTemplateColumns =  'repeat(16, 45px)';
+const numRows = 16;
+const numCols = 16;
+
+const squareWidth = 45;
+
+for (let i = 0; i < numRows * numCols; i++) {
+    const div = document.createElement('div'); 
+    div.style.border = '2px solid black';
+    div.style.width = `${squareWidth}px`;
+    div.style.height = `${squareWidth}px`;
+
+    target.appendChild(div);
 }
